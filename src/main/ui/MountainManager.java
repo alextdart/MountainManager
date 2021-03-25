1d12a827c1d99f5d12c351070a045280b3a57dfc
alexdart <alexdart@students.cs.ubc.ca>
2021-03-25 14:29:35 -0700

    added audio/visual component in the form of mountain background image under title text

--- src/main/ui/MountainManager.java
+++ src/main/ui/MountainManager.java
@@ -24,7 +24,7 @@ public class MountainManager extends JFrame implements ActionListener {
     private JsonWriter jsonWriter;
     private JsonReader jsonReader;
     private static final String JSON_STORE = "./data/resort.json"; // borrowed from JsonDemo
-    private static final String MTNS_URL = "./data/mountains.jpg";
+    private static final String MOUNTAINS_URL = "./data/mountains.jpg";
 
     // Constructs main window
     // EFFECTS: initializes with blank resort, starts fields and graphics
@@ -305,7 +305,7 @@ public class MountainManager extends JFrame implements ActionListener {
     // EFFECTS: creates the label for the mountain title at the top of the header
     private JLabel leftText(String text) {
         JLabel l = new JLabel();
-        ImageIcon icon = new ImageIcon(MTNS_URL);
+        ImageIcon icon = new ImageIcon(MOUNTAINS_URL);
         l.setIcon(icon);
         l.setText(text);
         l.setHorizontalTextPosition(JLabel.CENTER);
